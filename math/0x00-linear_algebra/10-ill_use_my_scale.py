1d3b27b00428323cae50a9f4cf9b04baa85e0080
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-12-02 22:53:42 -0500

    First version

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Calculates the shape of a matrix """
+
+
+def np_shape(matrix):
+    """a function that calculates the shape of a numpy.ndarray"""
+    return matrix.shape
