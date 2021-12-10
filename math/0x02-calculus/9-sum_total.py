15f6427c5983427a2317cd2c793558faf5ecc608
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-12-09 23:40:37 -0500

    Fixing PEP8

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
-"""Our life is the sum total of all the decisions we make every day, 
-   and those decisions are determined by our priorities"""
+"""
+Our life is the sum total
+of all the decisions we make
+every day, and those decisions
+are determined by our priorities
+"""
 
 
 def summation_i_squared(n):
