189d58003684a653c1efc24455933d76067f025e
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-12-09 23:45:19 -0500

    Fixing PEP8

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -3,7 +3,7 @@
 
 
 def summation_i_squared(n):
-    """a function that calculates \sum_{i=1}^{n} i^2"""
+    """a function that calculates"""
     if type(n) != int or n < 1:
         return None
     result = int((n * (n + 1) * (2 * n + 1)) / 6)
