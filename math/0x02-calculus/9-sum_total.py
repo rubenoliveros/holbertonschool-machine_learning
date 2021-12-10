657979ca50f9af0ca7d36b032feb56ed28802123
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-12-09 23:04:30 -0500

    Casting result to int

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -5,7 +5,7 @@
 
 def summation_i_squared(n):
     """a function that calculates \sum_{i=1}^{n} i^2"""
-    if  n < 1 or type(n) != int:
+    if  type(n) != int or n < 1:
         return None
     result = int((n * (n + 1) * (2 * n + 1)) // 6)
     return result
