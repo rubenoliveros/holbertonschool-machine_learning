01d621c9ccbf696b9be33266e903dd99562c1e32
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-12-09 23:44:40 -0500

    Fixing PEP8

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
-"""
-Our life is the sum total
-of all the decisions we make
-every day, and those decisions
-are determined by our priorities
-"""
+"""Our life is the sum total"""
 
 
 def summation_i_squared(n):
     """a function that calculates \sum_{i=1}^{n} i^2"""
     if type(n) != int or n < 1:
         return None
-    result = n * (n + 1) * (2 * n + 1)) / 6
+    result = int((n * (n + 1) * (2 * n + 1)) / 6)
     return result
