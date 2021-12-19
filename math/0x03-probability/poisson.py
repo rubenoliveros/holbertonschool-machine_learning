b00bea3b7422675b87653d024352766415656231
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-12-19 16:58:05 -0500

    Third version

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -34,3 +34,17 @@ class Poisson():
 
             pmf = e ** -self.lambtha * self.lambtha ** k / k_factorial
             return pmf
+
+    def cdf(self, k):
+        """Calculates the value of the CDF for a given number of successes"""
+        e = 2.7182818285
+        k = int(k)
+        if k < 0:
+            return 0
+        
+        cdf = 0
+        for i in range(k + 1):
+            cdf += self.pmf(i)
+        return cdf
+
+
