957811cb60ca6d45269b3639041deb41e4053fae
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-12-19 18:45:59 -0500

    Second version

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -20,3 +20,23 @@ class Exponential():
                     self.lambtha = float(1 / (sum(self.data) / len(self.data)))
             else:
                 raise TypeError("data must be a list")
+
+    def pdf(self, k):
+        """Calculates the value of the PDF for a given time period"""
+        e = 2.7182818285
+        k = int(k)
+        if k < 0:
+            return 0
+        else:
+            pdf = self.lambtha * e ** (-self.lambtha * k)
+            return pdf
+
+#    def cdf(self, k):
+#        """Calculates the value of the CDF for a given number of successes"""
+#        k = int(k)
+#        if k < 0:
+#            return 0
+#        cdf = 0
+#        for i in range(k + 1):
+#            cdf += self.pmf(i)
+#        return cdf
