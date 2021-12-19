6289097ad4af9e7a05f6f9a5f605ef5040341849
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-12-19 16:44:51 -0500

    Second version

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -20,3 +20,45 @@ class Poisson():
                     self.lambtha = float(sum(self.data)/len(self.data))
             else:
                 raise TypeError("data must be a list")
+
+    def pmf(self, k):
+        """Calculates the value of the PMF for a given number of successes"""
+        e = 2.7182818285
+        k = int(k)
+        if k < 0:
+            return 0
+        else:
+            k_factorial = 1
+            for i in range(1, k + 1):
+                k_factorial = k_factorial * i
+
+            pmf = e ** -self.lambtha * self.lambtha ** k / k_factorial
+            return pmf
+
+#def pmf(self, k):
+#    """Calculates the value of the PMF for a given number of successes"""
+#        if k < 0:
+#            return 0
+#
+#        k = int(k)
+#        fact = 1
+#        for i in range(1, k + 1):
+#            fact = fact * i
+#
+#        pmf = ((self.e ** (- self.lambtha)) * (self.lambtha ** k)) / fact
+#        return pmf
+#
+#    def pmf(self, k):
+#        """Calculates the value of the PMF for a given number of “successes”"""
+#        k = int(k)
+#
+#        if k < 0:
+#            return 0
+#
+#        # Multiply elements one by one
+#        factorial = 1
+#        for x in range(1, k + 1):
+#            factorial *= x
+#
+#        return ((2.7182818285 ** (- self.lambtha)) *
+#                (self.lambtha ** k)) / factorial
