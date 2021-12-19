eff6b85d8bfadf6836228873cad4b4e9b629477c
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-12-19 03:24:46 -0500

    First version

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""0. Initialize Poisson"""
+
+
+class Poisson():
+    """a class that represents a poisson distribution"""
+
+    def __init__(self, data=None, lambtha=1.):
+        """Constructor for class Poison"""
+        self.lambtha = float(lambtha)
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+        else:
+            if isinstance(data, list):
+                if len(data) < 1:
+                    raise ValueError("data must contain multiple values")
+                else:
+                    self.data = data
+                    self.lambtha = float(sum(self.data) / len(self.data))
+            else:
+                raise TypeError("data must be a list")
