7b5dc758c790fa7483768bb1b21ba564101185b4
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-12-19 21:32:29 -0500

    Third version

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -34,3 +34,9 @@ class Normal():
     def x_value(self, z):
         """Calculates the x-value of a given z-score"""
         return z * self.stddev + self.mean
+
+    def pdf(self, x):
+        """Calculates the value of the PDF for a given x-value"""
+        constant = 1 / (self.stddev * (2 * pi) ** 0.5)
+        exponent = e ** -(((x - self.mean) ** 2) / (2 * self.stddev ** 2))
+        return constant * exponent
