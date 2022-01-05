6f75ecc6b9f43202625b607a1fc6276e58f01a78
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-01-04 22:07:51 -0500

    First version

---
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""8. NeuralNetwork"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """a class that defines a neural network with one hidden layer"""
+    def __init__(self, nx, nodes):
+        """Initializes the class"""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) != int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
