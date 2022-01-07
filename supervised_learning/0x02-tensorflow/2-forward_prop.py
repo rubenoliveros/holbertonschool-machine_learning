5cdb6c390a05b5fc2fe0613978432d692d8edda2
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-01-06 22:44:36 -0500

    Tried another approach

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -6,7 +6,10 @@ create_layer = __import__('1-create_layer').create_layer
 
 def forward_prop(x, layer_sizes=[], activations=[]):
     """A function that creates the forward propagation graph for the NN"""
-    layer = create_layer(x, layer_sizes[0], activations[0])
-    for i in range(1 - len(layer_sizes)):
-        layer = create_layer(layer, layer_sizes[i], activations[i])
-    return layer
+    for i in range(len(layer_sizes)):
+        if i == 0:
+            prediction = create_layer(x, layer_sizes[0], activations[0])
+        else:
+            prediction = create_layer(prediction, layer_sizes[i],
+                                      activations[i])
+    return prediction
