e5198f27dc00388847e338b7b8030d0db49d7280
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-01-06 21:54:36 -0500

    Had to reinstall Keras

---
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""2. Forward Propagation"""
+import tensorflow as tf
+create_layer = __import__('1-create_layer').create_layer
+
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+    """A function that creates the forward propagation graph for the NN"""
+    layer = create_layer(x, layer_sizes[0], activations[0])
+    for i in range(1, len(layer_sizes)):
+        layer = create_layer(layer, layer_sizes[i], activations[i])
+    return layer
