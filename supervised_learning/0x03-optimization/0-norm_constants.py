105d8c9ada534c0a6acbd9ed974e74d8a0b30005
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-01-13 19:56:39 -0500

    First version

--- supervised_learning/0x03-optimization/0-norm_constants.py
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
+"""0. Normalization Constants"""
+import numpy as np
 
 
+def normalization_constants(X):
+    """A  function that calculates the normalization constants of a matrix"""
+    mean = np.mean(X, axis=0)
+    stddev = np.std(X, axis=0)
+    return mean, stddev
