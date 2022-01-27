ecdf2104f8c061c6502c0b23f68c2171248f45bc
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-01-26 22:31:57 -0500

    Fixing for PEP8

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -8,14 +8,14 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     reg = k.regularizers.l2(lambtha)
     inpt = k.layers.Input(shape=(nx,))
     layer = k.layers.Dense(layers[0],
-            input_shape=(nx,),
-            activation=activations[0],
-            kernel_regularizer=reg)(inpt)
-    for i in range(1, len(layers)):
+                           input_shape=(nx,),
+                           activation=activations[0],
+                           kernel_regularizer=reg)(inpt)
+    for i in range(0, len(layers)):
         layer = k.layers.Dropout(rate=1-keep_prob)(layer)
         layer = k.layers.Dense(layers[i],
-                activation=activations[i],
-                kernel_regularizer=reg)(layer)
+                               activation=activations[i],
+                               kernel_regularizer=reg)(layer)
     model = k.Model(inputs=inpt, outputs=layer)
 
     return model
