bb0f23dfa9a25ded789b6f4d0c27444ff86333a4
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-01-27 21:44:36 -0500

    Second version

--- math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -8,19 +8,21 @@ def convolve_grayscale_padding(images, kernel, padding):
     A function that performs a same convolution on grayscale 
     images with custom padding
     """
-    m, h, w = images.shape[0], images.shape[1], images.shape[2]
-    kh, kw = kernel.shape[0], kernel.shape[1]
-    pad_h, pad_w = padding[0], padding[1]
-    y_h = h + 2 * pad_h - kh + 1
-    y_w = w + 2 * pad_w - kw + 1
+    x_w, x_h, m = images.shape[2], images.shape[1], images.shape[0]
+    kernel_w, kernel_h = kernel.shape[1], kernel.shape[0]
+    pad_w, pad_h = padding[1], padding[0]
+    y_w = x_w + 2 * pad_w - kernel_w + 1
+    y_h = x_h + 2 * pad_h - kernel_h + 1
 
     image = np.pad(images, pad_width=((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
                    mode='constant')
 
-    c_i = np.zeros((m, y_h, y_w))
+    y = np.zeros((m, y_h, y_w))
 
     for j in range(y_w):
         for i in range(y_h):
-            c_i[:, i, j] = (kernel *
-                            images[:, i: i + kh, j: j + kw]).sum(axis=(1, 2))
-    return c_i
+            y[:, i, j] = (kernel *
+                          image[:, i: i + kernel_h,
+                                j: j + kernel_w]).sum(axis=(1, 2))
+
+    return y
