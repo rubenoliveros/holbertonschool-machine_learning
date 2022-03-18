67a08b7e4ceb84de0d7701edfb826fdf615b0f58
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-03-17 23:22:51 -0500

    Second version

---
+++ math/0x05-advanced_linear_algebra/4-inverse.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+"""4. Inverse"""
+
+
+def determinant(matrix):
+    """A function that calculates the determinant of a matrix"""
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a list of lists")
+
+    if matrix == [[]]:
+        return 1
+
+    for i in range(len(matrix)):
+        if len(matrix) != len(matrix[i]):
+            raise ValueError("matrix must be a square matrix")
+        if type(matrix[i]) is not list or not len(matrix[i]):
+            raise TypeError("matrix must be a list of lists")
+
+    if len(matrix) == 1 and len(matrix[0]) == 1:
+        return matrix[0][0]
+
+    if len(matrix) == 2 and len(matrix[0]) == 2:
+        return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0])
+
+    first_row = matrix[0]
+    determ = 0
+    cof = 1
+    for i in range(len(matrix[0])):
+        next_matrix = [x[:] for x in matrix]
+        del next_matrix[0]
+        for mat in next_matrix:
+            del mat[i]
+        determ += first_row[i] * determinant(next_matrix) * cof
+        cof = cof * -1
+
+    return determ
+
+
+def cofactor(matrix):
+    """A function that calculates the cofactor matrix of a matrix"""
+    if type(matrix) is not list or not len(matrix):
+        raise TypeError("matrix must be a list of lists")
+
+    if matrix == [[]]:
+        raise ValueError("matrix must be a non-empty square matrix")
+
+    for i in range(len(matrix)):
+        if len(matrix) != len(matrix[i]):
+            raise ValueError("matrix must be a non-empty square matrix")
+        if type(matrix[i]) is not list or not len(matrix[i]):
+            raise TypeError("matrix must be a list of lists")
+
+    if len(matrix) == 1:
+        return [[1]]
+
+    list_minor = []
+    for i in range(len(matrix)):
+        inner = []
+        if i % 2 == 0:
+            cof = 1
+        else:
+            cof = -1
+        for j in range(len(matrix[0])):
+            next_matrix = [x[:] for x in matrix]
+            del next_matrix[i]
+            for mat in next_matrix:
+                del mat[j]
+            determ = determinant(next_matrix) * cof
+            inner.append(determ)
+            cof = cof * -1
+        list_minor.append(inner)
+
+    return list_minor
+
+
+def adjugate(matrix):
+    """A function that calculates the adjugate matrix of a matrix"""
+    adj = cofactor(matrix)
+    transpose = []
+    for j in range(len(adj[0])):
+        inner = []
+        for i in range(len(adj)):
+            inner.append(adj[i][j])
+        transpose.append(inner)
+    return transpose
+
+
+def inverse(matrix):
+    """A function that calculates the inverse of a matrix"""
+    adjug = adjugate(matrix)
+    det = determinant(matrix)
+    if det == 0:
+        return None
+    inver = []
+    for i in range(len(matrix)):
+        inner = []
+        for j in range(len(matrix[0])):
+            inner.append(adjug[i][j] / det)
+        inver.append(inner)
+    return inver
