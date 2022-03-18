962087b0fcb808344d78846fc3c074ebc26d6462
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-03-17 23:23:10 -0500

    Fourth version

---
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""5. Definiteness"""
+import numpy as np
+
+
+def definiteness(matrix):
+    """A function that calculates the definiteness of a matrix"""
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("matrix must be a numpy.ndarray")
+    if len(matrix.shape) != 2:
+        return None
+    row, col = matrix.shape
+    if row != col:
+        return None
+    # Matrix has to be symmetric to calculate definiteness
+    if not (matrix == matrix.T).all():
+        return None
+    eigenvalues = np.linalg.eigvals(matrix)
+
+    pos = 0
+    neg = 0
+    semi = 0
+    for i in eigenvalues:
+        if i > 0:
+            pos = 1
+        if i < 0:
+            neg = 1
+        if i == 0:
+            semi = 1
+
+    if pos and not semi and not neg:
+        return "Positive definite"
+    elif pos and semi and not neg:
+        return "Positive semi-definite"
+    elif not pos and not semi and neg:
+        return "Negative definite"
+    elif not pos and semi and neg:
+        return "Negative semi-definite"
+    elif pos and not semi and neg:
+        return "Indefinite"
+    else:
+        return None
