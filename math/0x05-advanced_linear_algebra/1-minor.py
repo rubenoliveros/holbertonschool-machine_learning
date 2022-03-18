59a32359d448e3a5cd99611343e02b61f65eb3d7
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-03-17 23:52:57 -0500

    Fixing por PEP8

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -51,7 +51,7 @@ def minor(matrix):
             raise TypeError("matrix must be a list of lists")
     
     if len(matrix) == 1:
-        return 1
+        return [[1]]
     
     minor = []
     for i in range(len(matrix)):
