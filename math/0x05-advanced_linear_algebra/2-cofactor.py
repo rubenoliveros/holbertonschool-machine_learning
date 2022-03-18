85bc48214e5b997b2bf4f18bdec9da68cee483b7
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-03-17 23:45:56 -0500

    Fixing for PEP8

--- math/0x05-advanced_linear_algebra/2-cofactor.py
+++ math/0x05-advanced_linear_algebra/2-cofactor.py
@@ -6,57 +6,68 @@ def determinant(matrix):
     """A function that calculates the determinant of a matrix"""
     if type(matrix) is not list:
         raise TypeError("matrix must be a list of lists")
+
     if matrix == [[]]:
         return 1
+
     for i in range(len(matrix)):
         if len(matrix) != len(matrix[i]):
             raise ValueError("matrix must be a square matrix")
         if type(matrix[i]) is not list or not len(matrix[i]):
             raise TypeError("matrix must be a list of lists")
+
     if len(matrix) == 1 and len(matrix[0]) == 1:
         return matrix[0][0]
+
     if len(matrix) == 2 and len(matrix[0]) == 2:
         return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0])
-    row = matrix[0]
-    det = 0
-    cofact = 1
+
+    first_row = matrix[0]
+    determ = 0
+    cof = 1
     for i in range(len(matrix[0])):
-        mat = [l[:] for l in matrix]
-        del mat[0]
-        for m in mat:
-            del m[i]
-        det += row[i] * determinant(mat) * cofact
-        cofact = cofact * -1
-    return det
+        next_matrix = [x[:] for x in matrix]
+        del next_matrix[0]
+        for mat in next_matrix:
+            del mat[i]
+        determ += first_row[i] * determinant(next_matrix) * cof
+        cof = cof * -1
+
+    return determ
 
 
 def cofactor(matrix):
     """A function that calculates the cofactor matrix of a matrix"""
     if type(matrix) is not list or not len(matrix):
         raise TypeError("matrix must be a list of lists")
+
     if matrix == [[]]:
-        raise ValueError("matrix must be a square matrix")
+        raise ValueError("matrix must be a non-empty square matrix")
+
     for i in range(len(matrix)):
         if len(matrix) != len(matrix[i]):
-            raise ValueError("matrix must be a square matrix")
+            raise ValueError("matrix must be a non-empty square matrix")
         if type(matrix[i]) is not list or not len(matrix[i]):
             raise TypeError("matrix must be a list of lists")
+
     if len(matrix) == 1:
         return [[1]]
-    minor = []
+
+    list_minor = []
     for i in range(len(matrix)):
         inner = []
-        if i % 2 != 0:
-            cofact = -1
+        if i % 2 == 0:
+            cof = 1
         else:
-            cofact = 1
+            cof = -1
         for j in range(len(matrix[0])):
-            mat = [l[:] for l in matrix]
-            del mat[i]
-            for m in mat:
-                del m[j]
-            det = determinant(mat) * cofact
-            inner.append(det)
-            cofact = cofact * (-1)
-        minor.append(inner)
-    return minor
+            next_matrix = [x[:] for x in matrix]
+            del next_matrix[i]
+            for mat in next_matrix:
+                del mat[j]
+            determ = determinant(next_matrix) * cof
+            inner.append(determ)
+            cof = cof * -1
+        list_minor.append(inner)
+
+    return list_minor
