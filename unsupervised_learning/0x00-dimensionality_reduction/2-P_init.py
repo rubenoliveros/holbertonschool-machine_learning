956e8901511af7b95c683b5a61e77a164fd98e70
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-03-24 06:52:55 -0500

    Fixing for PEP8

--- unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/0x00-dimensionality_reduction/2-P_init.py
@@ -6,15 +6,15 @@ import numpy as np
 
 
 def P_init(X, perplexity):
-   """
-   A function that initializes all variables
-   required to calculate the P affinities in t-SNE
-   """
-   n = X.shape[0]
-   mult = np.matmul(X, -X.T)
-   summation = np.sum(np.square(X), 1)
-   D = np.add(np.add(2 * mult, summation), summation.T)
-   P = np.zeros((n, n))
-   betas = np.ones((n, 1))
-   H = np.log2(perplexity)
-   return (D, P, betas, H)
+    """
+    A function that initializes all variables
+    required to calculate the P affinities in t-SNE
+    """
+    n = X.shape[0]
+    mult = np.matmul(X, -X.T)
+    summation = np.sum(np.square(X), 1)
+    D = np.add(np.add(2 * mult, summation), summation.T)
+    P = np.zeros((n, n))
+    betas = np.ones((n, 1))
+    H = np.log2(perplexity)
+    return (D, P, betas, H)
