eb47be0ba1e4ebe15464182f3ad8a3b88eb98b3c
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-03-24 05:49:56 -0500

    Second version

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -25,3 +25,21 @@ class MultiNormal:
         self.mean = mean
         cov = np.matmul(data - mean, data.T - mean.T) / (n - 1)
         self.cov = cov
+
+    def pdf(self, x):
+        """A public instance method that calculates the PDF at a data point"""
+        if type(x) is not np.ndarray:
+            raise TypeError("x must be a numpy.ndarray")
+        d = self.cov.shape[0]
+        if len(x.shape) != 2:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+        test_d, one = x.shape
+        if test_d != d or one != 1:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+        pdf = 1.0 / np.sqrt(((2 * np.pi) ** d) * det)
+        mult = np.matmul(np.matmul((x - self.mean).T, inv), (x - self.mean))
+        pdf *= np.exp(-0.5 * mult)
+        pdf = pdf[0][0]
+        return pdf
