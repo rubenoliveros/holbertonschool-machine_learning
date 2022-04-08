c621e4be4f8ca5f334ba29ac94ffa944f1697d5b
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-04-07 21:20:57 -0500

    First version

---
+++ unsupervised_learning/0x02-hmm/1-regular.py
@@ -0,0 +1,25 @@
+"""1. Regular Chains"""
+import numpy as np
+
+
+def regular(P):
+    """
+    Determines the steady state probabilities of a regular markov chain
+    Args:
+        P is a square 2D numpy.ndarray of shape (n, n) representing the
+          transition matrix
+            - P[i, j] is the probability of transitioning from state i to
+                      state j
+            - n is the number of states in the markov chain
+    Returns: a numpy.ndarray of shape (1, n) containing the steady state
+             probabilities, or None on failure
+    """
+    if len(P.shape) != 2 or P.shape[0] != P.shape[1] or P.shape[0] < 1:
+        return None
+
+    P = np.linalg.matrix_power(P, 100)
+    if np.any(P <= 0):
+        return None
+    ss_prob = np.array([P[0]])
+
+    return ss_prob
