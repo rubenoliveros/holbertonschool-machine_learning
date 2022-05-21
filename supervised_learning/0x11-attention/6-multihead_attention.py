842f9df33b1ba00052e2709547159f8b92c81404
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-05-21 18:57:01 -0500

    Second version

---
+++ supervised_learning/0x11-attention/6-multihead_attention.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""5. Multi Head Attention"""
+
+import tensorflow as tf
+sdp_attention = __import__('5-sdp_attention').sdp_attention
+
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+    """
+    A class that inherits from tensorflow.keras.layers.Layer
+    to perform multi head attention
+    """
+
+    def __init__(self, dm, h):
+        """
+        Class constructor
+        """
+        super(MultiHeadAttention, self).__init__()
+        self.h = h
+        self.dm = dm
+
+        self.depth = dm // h
+
+        self.Wq = tf.keras.layers.Dense(dm)
+        self.Wk = tf.keras.layers.Dense(dm)
+        self.Wv = tf.keras.layers.Dense(dm)
+
+        self.linear = tf.keras.layers.Dense(dm)
+
+    def split_heads(self, x, batch_size):
+        """Split the last dimension"""
+        x = tf.reshape(x, (batch_size, -1, self.h, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, Q, K, V, mask):
+        """
+        call function
+        """
+        batch_size = tf.shape(Q)[0]
+
+        q = self.Wq(Q)
+        k = self.Wk(K)
+        v = self.Wv(V)
+
+        q = self.split_heads(q, batch_size)
+        k = self.split_heads(k, batch_size)
+        v = self.split_heads(v, batch_size)
+
+        scl_att, attention_weights = sdp_attention(q, k, v, mask)
+
+        scl_att = tf.transpose(scl_att,
+                               perm=[0, 2, 1, 3])
+
+        concat_attention = tf.reshape(scl_att, (batch_size, -1,
+                                                self.dm))
+
+        output = self.linear(concat_attention)
+        return output, attention_weights
