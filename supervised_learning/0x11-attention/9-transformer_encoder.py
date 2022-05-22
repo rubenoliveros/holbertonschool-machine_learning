f7d06c6a4edd6d4781c1e3057fc89ec31ec018e8
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-05-21 19:35:29 -0500

    Second version

---
+++ supervised_learning/0x11-attention/9-transformer_encoder.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""8. Transformer Encoder"""
+
+import tensorflow as tf
+positional_encoding = __import__('4-positional_encoding').positional_encoding
+EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
+
+
+class Encoder(tf.keras.layers.Layer):
+    """
+    A class that inherits from tensorflow.keras.layers.Layer
+    to create the encoder for a transformer
+    """
+
+    def __init__(self, N, dm, h, hidden, input_vocab,
+                 max_seq_len, drop_rate=0.1):
+        """
+        Init Function
+        """
+        super().__init__()
+        self.N = N
+        self.dm = dm
+        self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
+        self.positional_encoding = positional_encoding(max_seq_len, self.dm)
+        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate)
+                       for _ in range(N)]
+        self.dropout = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, training, mask):
+        """
+        call function
+        """
+        seq_len = x.shape[1]
+        embedding = self.embedding(x)
+        embedding *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
+        embedding += self.positional_encoding[:seq_len]
+        encoder_out = self.dropout(embedding, training=training)
+
+        for i in range(self.N):
+            encoder_out = self.blocks[i](encoder_out, training, mask)
+
+        return encoder_out
