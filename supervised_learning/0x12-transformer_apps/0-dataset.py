702130f6102e1a4cdfdf57e903bbf6641f1a03f5
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-05-26 22:35:10 -0500

    First version

---
+++ supervised_learning/0x12-transformer_apps/0-dataset.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""0. Dataset"""
+import tensorflow.compat.v2 as tf
+import tensorflow_datasets as tfds
+
+
+class Dataset:
+    """ Class that loads and preps a dataset for machine translation """
