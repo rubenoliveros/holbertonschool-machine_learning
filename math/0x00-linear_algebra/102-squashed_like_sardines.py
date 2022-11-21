ac3fd26f2c1dbaaae5d829909e7ffe95515762fb
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-11-20 22:57:25 -0500

    Fixing for permissions

--- math/0x00-linear_algebra/102-squashed_like_sardines.py
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -2,37 +2,32 @@
 "17. Squashed Like Sardines"
 
 
-def cat_matrices(mat1, mat2, axis=0):
-    """Concatenates two matrices along a specific axis
-    Args:
-        mat1 (list): matrix of int/floats
-        mat2 list): matrix of int/floats
-        axis (int, optional): axis to perform concatenation. Defaults to 0.
-    Returns:
-        list: concatenated matrix. None if it can't be done
-    """
-    # check if columns are equals
-    if not axis and matrix_shape(mat1) == matrix_shape(mat2):
-        return mat1 + mat2
-    # check if rows are equals
-    if axis == 1 and len(mat1) == len(mat2):
-        return [mat1[row] + mat2[row] for row in range(len(mat1))]
-
-    if axis == 3 and len(mat1[0]) == len(mat2[0]):
-        return mat1 + mat2
-
-
 def matrix_shape(matrix):
-    """Calculates the shape of a matrix with the same dimension
-    Args:
-        matrix (list): matrix to be evaluated
-    Returns:
-        shape_matrix (list): list of integers desribing the matrix shape
-    """
-    # base case
-    if type(matrix) is int:
-        return []
+    """ returns list of integers representing dimensions of given matrix """
+    matrix_shape = []
+    while (type(matrix) is list):
+        matrix_shape.append(len(matrix))
+        matrix = matrix[0]
+    return matrix_shape
 
-    shape_matrix = matrix_shape(matrix[0])
-    shape_matrix.insert(0, len(matrix))
-    return shape_matrix
+
+def cat_matrices(mat1, mat2, axis=0):
+    """ returns concatenation of two matrices along a specific axis """
+    from copy import deepcopy
+    shape1 = matrix_shape(mat1)
+    shape2 = matrix_shape(mat2)
+    if len(shape1) != len(shape2):
+        return None
+    for i in range(len(shape1)):
+        if i != axis:
+            if shape1[i] != shape2[i]:
+                return None
+    return rec(deepcopy(mat1), deepcopy(mat2), axis, 0)
+
+
+def rec(m1, m2, axis=0, current=0):
+    """ recursively calls function until gets to level to extend """
+    if axis != current:
+        return [rec(m1[i], m2[i], axis, current + 1) for i in range(len(m1))]
+    m1.extend(m2)
+    return m1
