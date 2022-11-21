5b67a513012a2271a1adaea0d52d08d7efee0374
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-11-20 23:55:42 -0500

    Fixing for permissions

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -5,36 +5,22 @@
 
 
 def poly_integral(poly, C=0):
-    """
-    poly_integral(poly, C=0) - calculates the integral of a polynomial
-    @poly: is a list of coefficients representing a polynomial.
-           The index of the list represents the power of x that
-           the coefficient belongs to.
-    @C: is an integer representing the integration constant.
-    Returns: a new list of coefficients representing the integral of
-             the polynomial.
-    """
+    """function to integrate a poly"""
 
-    if type(poly) is not list or type(C) not in [int, float]:
+    if type(poly) is not list:
         return None
-
-    res = [x / (idx + 1) for idx, x in enumerate(poly)]
-    res = [
-        int(x)
-        if type(x) is float and x.is_integer()
-        else x
-        for x in res
-    ]
-
-    if type(C) is float and C.is_integer():
-        C = int(C)
-
-    res.insert(0, C)
-
-    while res[-1] == 0:
-        del res[-1]
-
-    if len(res) == 0:
-        return [0, ]
-
-    return res
+    elif not poly:
+        return None
+    elif len(poly) == 0:
+        return None
+    elif type(C) is not int:
+        return None
+    elif poly == [0]:
+        return [C]
+    else:
+        integral = []
+        integral.append(C)
+        for i in range(len(poly)):
+            x = poly[i] / (i + 1)
+            integral.append(int(x) if x.is_integer() else x)
+        return integral
