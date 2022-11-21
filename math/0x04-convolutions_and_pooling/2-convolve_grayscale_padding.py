2436376782dca4e62bc1681f47c06540404c4a1c
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-11-21 01:41:14 -0500

    Fixing for permissions

--- math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -1,28 +1,17 @@
 #!/usr/bin/env python3
-"""2. Convolution with Padding"""
+"""performs a convolution on grayscale images with custom padding"""
 import numpy as np
 
 
 def convolve_grayscale_padding(images, kernel, padding):
-    """
-    A function that performs a same convolution on grayscale 
-    images with custom padding
-    """
-    x_w, x_h, m = images.shape[2], images.shape[1], images.shape[0]
-    kernel_w, kernel_h = kernel.shape[1], kernel.shape[0]
-    pad_w, pad_h = padding[1], padding[0]
-    y_w = x_w + 2 * pad_w - kernel_w + 1
-    y_h = x_h + 2 * pad_h - kernel_h + 1
-
-    image = np.pad(images, pad_width=((0, 0), (pad_h, pad_h), (pad_w, pad_w)),
-                   mode='constant')
-
-    y = np.zeros((m, y_h, y_w))
-
-    for j in range(y_w):
-        for i in range(y_h):
-            y[:, i, j] = (kernel *
-                          image[:, i: i + kernel_h,
-                                j: j + kernel_w]).sum(axis=(1, 2))
-
-    return y
+    """performs a convolution on grayscale images with custom padding"""
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    ph, pw = padding
+    out_images = np.zeros((m, h - kh + (2 * ph) + 1, w - kw + (2 * pw) + 1))
+    img = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant')
+    for i in range(h - kh + (2 * ph) + 1):
+        for j in range(w - kw + (2 * pw) + 1):
+            out_images[:, i, j] = np.sum(
+                kernel * img[:, i: i + kh, j: j + kw], axis=(1, 2))
+    return out_images
