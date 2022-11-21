32e12dda39ffa4e33242012f5b07de64735a0092
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-11-21 00:29:41 -0500

    Fixing for permissions

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -2,38 +2,42 @@
 """4. Exponential PDF"""
 
 
-class Exponential():
-    """a class that represents an exponential distribution"""
+class Exponential:
+    """define class"""
+
+    e = 2.7182818285
+
     def __init__(self, data=None, lambtha=1.):
-        """Constructor for class Exponential"""
-        e = 2.7182818285
-        if data is None:
+        """class constructor"""
+        if data is None and isinstance(lambtha, (float, int)):
             if lambtha <= 0:
-                raise ValueError("lambtha must be a positive value")
-            else:
-                self.lambtha = float(lambtha)
-        else:
-            if isinstance(data, list):
-                if len(data) < 2:
-                    raise ValueError("data must contain multiple values")
-                else:
-                    self.data = data
-                    self.lambtha = float(1/(sum(self.data)/len(self.data)))
-            else:
-                raise TypeError("data must be a list")
+                raise ValueError('lambtha must be a positive value')
+            self.lambtha = float(lambtha)
+        elif data is not None:
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            self.lambtha = float(1 / (sum(data) / len(data)))
 
     def pdf(self, x):
-        """Calculates the value of the PDF for a given time period"""
-        k = int(x)
-        if k < 0:
+        """
+        function that calculates the probability density function
+        for a given time period x
+        """
+        if x is None or x < 0:
             return 0
-        else:
-            return self.lambtha * (e ** (-self.lambtha * k))
+        return (self.lambtha * (
+            Exponential.e ** ((-1 * self.lambtha) * x)
+        ))
 
     def cdf(self, x):
-        """ Calculates the value of the CDF for a given time period"""
-        k = int(x)
-        if k < 0:
+        """
+        function that calculates the cumulative distribution function
+        for a given time period x
+        """
+        if x is None or x < 0:
             return 0
-        cdf = -e ** (-self.lambtha * k) + 1
-        return cdf
+        return (1 - (
+            Exponential.e ** ((-1 * self.lambtha) * x)
+        ))
