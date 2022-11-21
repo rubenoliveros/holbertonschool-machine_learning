bdece84aad3d566498942bbc597e41114febb430
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-11-20 22:50:25 -0500

    Fixing for permissions

--- math/0x00-linear_algebra/102-squashed_like_sardines.py
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -2,40 +2,54 @@
 "17. Squashed Like Sardines"
 
 
-def matrix_shape(matrix):
-    """
-    Calculates the shape of a matrix.
-    Parameters
-    ----------
-    matrix : array-like
-        The matrix to calculate the shape of
-    Returns
-    -------
-    int
-        The shape of the of matrix
+def shape(matrix):
+    """ return the shape of a matrix
+    Args:
+        matrix: Given matrix
+    Return:
+        the shape of the matrix: ndim
     """
     if type(matrix[0]) != list:
         return [len(matrix)]
     else:
-        return [len(matrix)] + matrix_shape(matrix[0])
+        return [len(matrix)] + shape(matrix[0])
 
 
-def matrix_creation(mat1, mat2, axis=0):
-    "If possible, creates a new matrix for concatenation"
-    retorno = []
-    if axis == 0:
-        retorno = mat1 + mat2
-        return retorno
-    for x in range(len(mat1)):
-        retorno.append(matrix_creation(mat1[x], mat2[x], axis - 1))
-    return retorno
+def rec_matrix(mat1, mat2, rank, axis=0):
+    """ recursively operate a concatenation of a n matrix
+        Args:
+            mat1, mat2: Given matrix
+            axis: Given axis
+            rank: Given rank to check if it is in the same
+            axis mat1 and mat2
+        Return:
+            the concatenation of mat1, mat2 iterating recursively: ndim
+        """
+    new_mat = []
+    if (type(mat1[0]) and type(mat2[0])) and rank == axis:
+        new_mat = [y for x in [mat1, mat2] for y in x]
+        return new_mat
+    else:
+        for x in range(len(mat1)):
+            if type(mat1[x]) == list:
+                new_mat = [sum(x) for x in zip(rec_matrix(mat1[x],
+                                                          mat2[x],
+                                                          rank + 1,
+                                                          axis))]
+            return new_mat
 
 
 def cat_matrices(mat1, mat2, axis=0):
-    "Concatenates two matrices"
-    uno = len(matrix_shape(mat1))
-    dos = len(matrix_shape(mat2))
-    if uno >= axis and dos >= axis and uno == dos:
-        return matrix_creation(mat1, mat2, axis)
-    else:
+    """ concatenate n dimesnional matrices with the same shape
+    Args:
+        mat1, mat2: Given matrix
+        axis: given axis
+    Return:
+        new_mat: Recursively concatenation of mat1 and mat2
+    """
+    if shape(mat1) != shape(mat2):
         return None
+    else:
+        rank = 0
+        new_mat = rec_matrix(mat1, mat2, rank, axis)
+        return new_mat
