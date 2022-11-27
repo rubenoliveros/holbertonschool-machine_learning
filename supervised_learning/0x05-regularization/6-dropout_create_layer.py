5dd99997b83f071603aead588508b35693289e04
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-11-27 00:58:04 -0500

    Fixing for PEP8

--- supervised_learning/0x05-regularization/6-dropout_create_layer.py
+++ supervised_learning/0x05-regularization/6-dropout_create_layer.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
-"""Neural network layer dropout Module"""
-# prev is a tensor containing the output of the previous layer
-# n is the number of nodes the new layer should contain
-# activation is the activation function that should be used on the layer
-# keep_prob is the probability that a node will be kept
+"""Function that creates a layer of a neural network using dropout"""
 import tensorflow as tf
 
 
 def dropout_create_layer(prev, n, activation, keep_prob):
-    """Function that returns output of the layer"""
-    w = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    Drop_reg = tf.layers.Dropout(keep_prob)
-    layer = tf.layers.Dense(units=n, activation=activation,
-                            kernel_initializer=w, name="layer",
-                            kernel_regularizer=Drop_reg)
-    y = layer(prev)
-    return (y)
+    """Function that creates a layer of a neural network using dropout"""
+    W = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    l2 = tf.layers.Dropout(keep_prob)
+    model = tf.layers.Dense(units=n, activation=activation,
+                            name="layer", kernel_initializer=W,
+                            kernel_regularizer=l2)
+    return model(prev)
