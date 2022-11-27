0994038aa41e969b168e3e7aec96012fbb51b257
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-11-26 23:52:28 -0500

    Fixinf for PEP8

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
 """7. Evaluate"""
 import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 
 def evaluate(X, Y, save_path):
-    """A function that evaluates the output of a neural network"""
-    with tf.Session() as trainer:
-        storer = tf.train.import_meta_graph(save_path + ".meta")
-        storer.restore(trainer, save_path)
-        x = tf.get_collection('x')[0]
-        y = tf.get_collection('y')[0]
-        y_pred = tf.get_collection('y_pred')[0]
-        accuracy = tf.get_collection('accuracy')[0]
-        loss = tf.get_collection('loss')[0]
-        y_eval = trainer.run(y_pred, feed_dict={x: X, y: Y})
-        acc_eval = trainer.run(accuracy, feed_dict={x: X, y: Y})
-        loss_eval = trainer.run(loss, feed_dict={x: X, y: Y})
-        return y_eval, acc_eval, loss_eval
+    """
+    Evaluate the output of the neural network
+    :X: is a numpy.ndarray containing the input data
+    :Y: is a numpy.ndarray containing the one-hot labels for X
+    :save_path: is the path to the TensorFlow model
+    """
+    with tf.Session() as sess:
+        save_NN = tf.train.import_meta_graph("{}.meta".format(save_path))
+        save_NN.restore(sess, save_path)
+
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+
+        y_pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
+
+        prediction = sess.run(y_pred, feed_dict={x: X, y: Y})
+        accuracy = sess.run(accuracy, feed_dict={x: X, y: Y})
+        loss = sess.run(loss, feed_dict={x: X, y: Y})
+        return prediction, accuracy, loss
