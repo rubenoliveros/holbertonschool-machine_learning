1c5aa4109487afe61912ecc96faa0ed51060c797
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-11-27 00:39:40 -0500

    Fixing for PEP8

--- supervised_learning/0x03-optimization/13-batch_norm.py
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
+"""Batch normalization"""
 
 
+import numpy as np
+
+
+def batch_norm(Z, gamma, beta, epsilon):
+    """Function that normalizes an unactivated output of a neural network using
+    batch normalization:
+    Z: is a numpy.ndarray of shape (m, n) that should be normalized
+    m: is the number of data points
+    n: is the number of features in Z
+    gamma: is a numpy.ndarray of shape (1, n) containing the scales used for
+    batch normalization
+    beta: is a numpy.ndarray of shape (1, n) containing the offsets used for
+    batch normalization
+    epsilon: is a small number used to avoid division by zero
+    Returns: the normalized Z matrix"""
+    m = Z.mean(0)
+    variance = Z.std(0) ** 2
+    z_normalized = (Z - m) / ((variance + epsilon) ** 0.5)
+    z_normalized = z_normalized * gamma + beta
+    return z_normalized
