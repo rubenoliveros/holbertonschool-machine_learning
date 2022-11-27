dbad2a17729b1d338b85894e6722eac6a92fb8f5
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-11-27 00:52:38 -0500

    Fixing for PEP8

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
-"""3. Create a Layer with L2 Regularization"""
-import tensorflow as tf
+'''tensorflow layer with l2 regularization'''
+
+import tensorflow.compat.v1 as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
-    """A function that creates a tensorflow layer that includes L2 
-    regularization"""
-    function = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    regular = tf.contrib.layers.l2_regularizer(lambtha)
-
-    layer = tf.layers.Dense(
-        units=n,
-        name='layer',
-        activation=activation,
-        kernel_initializer=function,
-        kernel_regularizer=regular)
+    '''
+    creates a tensorflow layer that includes L2 regulazation.
+    :prev: a tensor containing the output of the previous layer
+    :n: the number of nodes the new layer should contain
+    :activation: the activation function that should be used on the layer
+    :lambtha: the L2 regularization parameter
+    :returns: the output of the new layer
+    '''
+    init = tf.keras.initializers.VarianceScaling(scale=2.0, mode='fan_avg')
+    regularizer = tf.keras.regularizers.L2(lambtha)
+    layer = tf.layers.Dense(units=n, activation=activation,
+                            kernel_initializer=init,
+                            kernel_regularizer=regularizer)
     return layer(prev)
