d253cc2e90822ffcbac57f880092610da142c945
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-11-27 00:30:26 -0500

    Fixing for PEP8

--- supervised_learning/0x03-optimization/10-Adam.py
+++ supervised_learning/0x03-optimization/10-Adam.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
+"""Moving average"""
 
 
+import tensorflow.compat.v1 as tf
+
+
+def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+    """Function that creates the training operation for a neural network in
+    tensorflow using the Adam optimization algorithm:
+    loss is the loss of the network
+    alpha is the learning rate
+    beta1 is the weight used for the first moment
+    beta2 is the weight used for the second moment
+    epsilon is a small number to avoid division by zero
+    Returns: the Adam optimization operation"""
+    return tf.train.AdamOptimizer(alpha, beta1, beta2, epsilon).minimize(loss)
