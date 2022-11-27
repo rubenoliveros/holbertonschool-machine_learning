1e975acd2c406ec7ae8966ecd719ea8269d1516f
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-11-27 00:20:58 -0500

    Fixing for PEP8

--- supervised_learning/0x03-optimization/8-RMSProp.py
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
+"""Moving average"""
 
 
+import tensorflow.compat.v1 as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """Function that creates the training operation for a neural network in
+    tensorflow using the RMSProp optimization algorithm:
+    loss: is the loss of the network
+    alpha: is the learning rate
+    beta2: is the RMSProp weight
+    epsilon: is a small number to avoid division by zero
+    Returns: the RMSProp optimization operation"""
+    return tf.train.RMSPropOptimizer(alpha,
+                                     beta2, epsilon=epsilon).minimize(loss)
