f997e046f98844b3a583c43def35a44eafc724cf
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-11-27 00:54:25 -0500

    Fixing for PEP8

--- supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -1,25 +1,45 @@
 #!/usr/bin/env python3
-"""5. Gradient Descent with Dropout"""
+"""Dropout gradient descent"""
+
+
 import numpy as np
 
 
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
-    """A function that updates the weights of a neural network with Dropout
-    regularization using gradient descent"""
+    """Function that updates the weights of a neural network with Dropout
+    regularization using gradient descent:
+    Y: is a one-hot numpy.ndarray of shape (classes, m) that contains the
+    correct labels for the data
+        classes: is the number of classes
+        m: is the number of data points
+    weights: is a dictionary of the weights and biases of the neural network
+    cache: is a dictionary of the outputs and dropout masks of each layer of
+    the neural network
+    alpha: is the learning rate
+    keep_prob: is the probability that a node will be kept
+    L: is the number of layers of the network"""
     m = Y.shape[1]
-    for i in reversed(range(L)):
-        A = cache['A' + str(i + 1)]
-        A2 = cache['A' + str(i)]
-        if i == L - 1:
-            dz = A - Y
-            W = weights['W' + str(i + 1)]
-        else:
-            da = 1 - (A * A)
-            dz = np.matmul(W.T, dz)
-            dz = dz * da * cache['D{}'.format(i + 1)]
-            dz /= keep_prob
-            W = weights['W' + str(i + 1)]
-        dw = np.matmul(A2, dz.T) / m
+    la = L
+    a = 'A' + str(la)
+    W = 'W' + str(la)
+    b = 'b' + str(la)
+    dz = cache[a] - Y
+    dw = (np.dot(cache['A' + str(la - 1)], dz.T) / m).T
+    db = np.sum(dz, axis=1, keepdims=True) / m
+    weights[W] = weights[W] - alpha * dw
+    weights[b] = weights[b] - alpha * db
+
+    for la in range(L - 1, 0, -1):
+        a = 'A' + str(la)
+        W = 'W' + str(la)
+        b = 'b' + str(la)
+        wNext = 'W' + str(la + 1)
+        aNext = 'A' + str(la - 1)
+        g = (1 - cache[a]**2)
+        dz = np.dot(weights[wNext].T,
+                    dz) * g * cache['D' + str(la)] / keep_prob
+        dw = (np.dot(cache[aNext], dz.T) / m).T
         db = np.sum(dz, axis=1, keepdims=True) / m
-        weights['W' + str(i + 1)] = weights['W' + str(i + 1)] - alpha * dw.T
-        weights['b' + str(i + 1)] = weights['b' + str(i + 1)] - alpha * db
+
+        weights[W] = weights[W] - alpha * dw
+        weights[b] = weights[b] - alpha * db
