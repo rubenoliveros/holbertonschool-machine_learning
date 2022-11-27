0b2253f72caa5636dc292dc7ecb2a713dcfb4da9
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-11-27 00:06:47 -0500

    Fixing for PEP8

--- supervised_learning/0x03-optimization/4-moving_average.py
+++ supervised_learning/0x03-optimization/4-moving_average.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
+"""Moving average"""
 
 
+import numpy as np
+
+
+def moving_average(data, beta):
+    """Function that calculates the weighted moving average of a data set:
+    data: is the list of data to calculate the moving average of
+    beta: is the weight used for the moving average
+    Returns: a list containing the moving averages of data"""
+    beta_prev = 0
+    moving_average_data = []
+    for i in range(len(data)):
+        beta_prev = beta_prev * beta + ((1 - beta) * data[i])
+        bias_correction = 1 - (beta ** (i + 1))
+
+        moving_average_data.append(beta_prev / bias_correction)
+    return moving_average_data
