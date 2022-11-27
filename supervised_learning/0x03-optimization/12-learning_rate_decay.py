21aecdae3b428408342fe0943cd9706ccc079ba7
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-11-27 00:37:43 -0500

    Fixing for PEP8

--- supervised_learning/0x03-optimization/12-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
+"""Moving average"""
 
 
+import tensorflow.compat.v1 as tf
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """Function that creates a learning rate decay operation in tensorflow
+    using inverse time decay:
+    alpha: is the original learning rate
+    decay_rate: is the weight used to determine the rate at which alpha will
+    decay
+    global_step: is the number of passes of gradient descent that have elapsed
+    decay_step: is the number of passes of gradient descent that should occur
+    before alpha is decayed further
+    Returns: the learning rate decay operation"""
+    return tf.train.inverse_time_decay(alpha, global_step, decay_step,
+                                       decay_rate, staircase=True)
