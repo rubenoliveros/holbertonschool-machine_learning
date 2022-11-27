2eb7de4650f8108d71f161b14ec9cde0ab4d76b9
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-11-27 00:15:50 -0500

    Fixing for PEP8

--- supervised_learning/0x03-optimization/6-momentum.py
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
+"""Moving average"""
 
 
+import tensorflow.compat.v1 as tf
+
+
+def create_momentum_op(loss, alpha, beta1):
+    """Function that creates the training operation for a neural network in
+    tensorflow using the gradient descent with momentum optimization algorithm
+    loss: is the loss of the network
+    alpha: is the learning rate
+    beta1: is the momentum weight
+    Returns: the momentum optimization operation"""
+    return tf.train.MomentumOptimizer(alpha, beta1).minimize(loss)
