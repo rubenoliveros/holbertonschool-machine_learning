12f1be98b863d2b35124ac899cf50e0e9c882f58
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-11-27 23:49:14 -0500

    Fixing for PEP8

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -1,20 +1,31 @@
 #!/usr/bin/env python3
-"""5. Validate"""
-import tensorflow.keras as k
+"""4-train"""
 
 
-def train_model(network,
-                data,
-                labels,
-                batch_size,
-                epochs,
-                verbose=True,
-                shuffle=False):
-    """Updates the function on 4-train.py to also analyze validation data"""
-    return network.fit(data,
-                       labels,
-                       batch_size=batch_size,
-                       epochs=epochs,
-                       validation_data=validation_data,
-                       verbose=verbose,
-                       shuffle=shuffle)
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """Function that trains the model
+    network: is the model to train
+    data: is a numpy.ndarray of shape (m, nx) containing the input data
+    labels: is a one-hot numpy.ndarray of shape (m, classes) containing the
+    labels of data
+    batch_size: is the size of the batch used for mini-batch gradient descent
+    epochs: is the number of passes through data for mini-batch gradient
+    descent
+    verbose: is a boolean that determines if output should be printed during
+    training
+    shuffle: is a boolean that determines whether to shuffle the batches every
+    epoch. Normally, it is a good idea to shuffle, but for reproducibility, we
+    have chosen to set the default to False.
+    Returns: the History object generated after training the model"""
+    if validation_data:
+        history = network.fit(
+            data, labels, batch_size, epochs, verbose, shuffle=shuffle,
+            validation_data=(validation_data))
+    else:
+        history = network.fit(
+            data, labels, batch_size, epochs, verbose, shuffle=shuffle)
+    return history
