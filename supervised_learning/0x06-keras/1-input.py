f7e4c1d199947de18b029a701ee784ba2d98b9f5
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-11-27 23:44:55 -0500

    Fixing for PEP8

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
-"""1. Input"""
-import tensorflow.keras as k
+"""1-Input"""
+
+
+import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    """A function that builds a neural network with the Keras library"""
-    reg = k.regularizers.l2(lambtha)
-    inpt = k.layers.Input(shape=(nx,))
-    layer = k.layers.Dense(layers[0],
-                           input_shape=(nx,),
-                           activation=activations[0],
-                           kernel_regularizer=reg)(inpt)
-    for i in range(0, len(layers)):
-        layer = k.layers.Dropout(rate=1-keep_prob)(layer)
-        layer = k.layers.Dense(layers[i],
-                               activation=activations[i],
-                               kernel_regularizer=reg)(layer)
-    model = k.Model(inputs=inpt, outputs=layer)
-
-    return model
+    """Function that build a keras model
+    nx is the number of input features to the network
+    layers is a list containing the number of nodes in each layer of the
+    network
+    activations is a list containing the activation functions used for each
+    layer of the network
+    lambtha is the L2 regularization parameter
+    keep_prob is the probability that a node will be kept for dropout
+    Return: keras model"""
+    regularizer = K.regularizers.L2(l2=lambtha)
+    inputLayer = K.Input(shape=(nx,), name='input_1')
+    lyr = K.layers.Dense(
+        layers[0], activation=activations[0],
+        kernel_regularizer=regularizer)(inputLayer)
+    for i in range(1, len(layers)):
+        dropoutLayer = K.layers.Dropout(1 - keep_prob)(lyr)
+        lyr = K.layers.Dense(
+            layers[i], activation=activations[i],
+            kernel_regularizer=regularizer)(dropoutLayer)
+    return K.Model(inputLayer, lyr)
