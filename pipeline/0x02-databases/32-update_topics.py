fb05d2935fa902b49aa68568e754690aedb4b272
Ruben Oliveros <ruben.oliveros@gmail.com>
2022-12-04 15:08:50 -0500

    First version

---
+++ pipeline/0x02-databases/32-update_topics.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Defines function that changes all topics of a school document based on name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name
+    parameters:
+        mongo_collection [pymongo]:
+            the MongoDB collection to use
+        name [string]:
+            the school name to update
+        topics [list of strings]:
+            list of topics approached in the school
+    """
+    mongo_collection.update_many({'name': name},
+                                 {'$set': {'topics': topics}})
